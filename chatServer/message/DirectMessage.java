40aa3786d18e7151a919694004324e0322bb6443
Anthony <anthony.kulis@gmail.com>
2017-01-13 11:22:33 -0600

    direct message built

--- chatServer/message/DirectMessage.java
+++ chatServer/message/DirectMessage.java
@@ -1,6 +1,48 @@
 package chatServer.message;
 
-public class DirectMessage {
-	public static class RoomMessage {
+import chatServer.user.User;
+
+public class DirectMessage extends Message {
+
+	private User from;
+	private User to;
+	private boolean sent;
+
+	private DirectMessage(){
+		this.from = null;
+		this.to = null;
+		this.sent = true;
+	}
+
+	public DirectMessage(User from, User to, String message){
+		super(message);
+		this.from = from;
+		this.to = to;
+	}
+
+	public User getFrom() {
+		return from;
+	}
+
+	public User getTo() {
+		return to;
+	}
+
+	public boolean isSent() {
+		return sent;
+	}
+
+	/* Tests */
+	public static void main(String... args){
+
+		User from = new User("anthony");
+		User to = new User("tom");
+		String message = "test message";
+
+		DirectMessage directMessage = new DirectMessage(from, to, message);
+
+		assert from == directMessage.getFrom() : "from is not who sent it";
+		assert to == directMessage.getTo() : "to is not who should receive it";
+		assert !directMessage.isSent() : "some how the message has been sent when there is no reason to send it";
 	}
 }
