8785518df577ce1736193ee6f6355939dd2c5c32
Anthony <anthony.kulis@gmail.com>
2017-02-15 10:34:28 -0600

    stubs pin number test

---
+++ edu/jalc/inclass/cocacola/security/PinNumberTest.java
@@ -0,0 +1,24 @@
+package edu.jalc.inclass.cocacola.security;
+
+public class PinNumberTest {
+
+	public void testGetPin(){
+		System.out.println("Testing PinNumber::getPin");
+		assert(false);
+	}
+
+	public void testGetExpirationDate(){
+		System.out.println("Testing PinNumber::getPin");
+		assert(false);
+	}
+
+
+	public static void main(String... args){
+		PinNumberTest pinNumberTest = new PinNumberTest();
+		pinNumberTest.testGetPin();
+		pinNumberTest.testGetExpirationDate();
+		System.out.println("Completed testing PinNumber");
+
+	}
+
+}
