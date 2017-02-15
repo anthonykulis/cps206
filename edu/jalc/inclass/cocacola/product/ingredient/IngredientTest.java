f0244d28640c42d00f9f177b465a952fa12fb3c2
Anthony <anthony.kulis@gmail.com>
2017-02-15 10:40:46 -0600

    stubs ingredient test

---
+++ edu/jalc/inclass/cocacola/product/ingredient/IngredientTest.java
@@ -0,0 +1,21 @@
+package edu.jalc.inclass.cocacola.product.ingredient;
+
+public class IngredientTest {
+
+	public void testGetName(){
+		System.out.println("Testing Ingredient::getName");
+		assert(false);
+	}
+
+	public void testtoString(){
+		System.out.println("Testing Ingredient::toString");
+		assert(false);
+	}
+
+	public static void main(String... args){
+		IngredientTest ingredientTest = new IngredientTest();
+		ingredientTest.testGetName();
+		ingredientTest.testtoString();
+		System.out.println("Completed testing Ingredients");
+	}
+}
