6d85e573398651315f58aa64e0d0c316bce66109
Anthony <anthony.kulis@gmail.com>
2017-02-15 10:43:11 -0600

    stubs soda bottle filler test

---
+++ edu/jalc/inclass/cocacola/manufacturing/SodaBottleFillerTest.java
@@ -0,0 +1,21 @@
+package edu.jalc.inclass.cocacola.manufacturing;
+
+public class SodaBottleFillerTest {
+
+	public void testGetInstance(){
+		System.out.println("Testing SodaBottleFiller::getInstance");
+		assert(false);
+	}
+
+	public void testFillSoda(){
+		System.out.println("Testing SodaBottleFiller::fillSoda");
+		assert(false);
+	}
+
+	public static void main(String... args){
+		SodaBottleFillerTest sodaBottleFillerTest = new SodaBottleFillerTest();
+		sodaBottleFillerTest.testGetInstance();
+		sodaBottleFillerTest.testFillSoda();
+		System.out.println("Completed testing SodaBottleFiller");
+	}
+}
