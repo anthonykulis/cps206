285cd076eb03d8d0cfef2eb2246a07a9d1bc2292
Anthony <anthony.kulis@gmail.com>
2017-02-15 10:38:59 -0600

    stubs recipe test

---
+++ edu/jalc/inclass/cocacola/recipe/RecipeTest.java
@@ -0,0 +1,33 @@
+package edu.jalc.inclass.cocacola.recipe;
+
+public class RecipeTest {
+
+	public void testGetIngredients(){
+		System.out.println("Testing Recipe::getIngredients");
+		assert(false);
+	}
+
+	public void testGetName(){
+		System.out.println("Testing Recipe::getName");
+		assert(false);
+	}
+
+	public void testGetInstructions(){
+		System.out.println("Testing Recipe::getInstructions");
+		assert(false);
+	}
+
+	public void testToString(){
+		System.out.println("Testing Recipe::toString");
+		assert(false);
+	}
+
+	public static void main(String... args){
+		RecipeTest recipeTest = new RecipeTest();
+		recipeTest.testGetIngredients();
+		recipeTest.testGetInstructions();
+		recipeTest.testGetName();
+		recipeTest.testToString();
+		System.out.println("Recipe tests completed");
+	}
+}
