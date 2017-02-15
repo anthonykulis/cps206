926ec7fca0ccd1890684599dc312c32e1db55cd6
Anthony <anthony.kulis@gmail.com>
2017-02-15 11:50:36 -0600

    provides solution

--- edu/jalc/inclass/cocacola/recipe/store/SecretRecipeStore.java
+++ edu/jalc/inclass/cocacola/recipe/store/SecretRecipeStore.java
@@ -1,6 +1,11 @@
 package edu.jalc.inclass.cocacola.recipe.store;
 
 
+import edu.jalc.inclass.cocacola.employee.Employee;
+import edu.jalc.inclass.cocacola.recipe.Recipe;
+import edu.jalc.inclass.cocacola.security.PinNumber;
+
+import java.util.HashMap;
 
 /*
 
@@ -13,4 +18,41 @@ package edu.jalc.inclass.cocacola.recipe.store;
 		match, throw an exception.
  */
 public class SecretRecipeStore {
+
+	private static SecretRecipeStore secretRecipeStore = null;
+	private final HashMap<Recipe, PinNumber> store;
+
+	private SecretRecipeStore(){
+		this.store = new HashMap<>();
+	}
+
+	public static SecretRecipeStore getInstance(){
+		if(secretRecipeStore == null) secretRecipeStore = new SecretRecipeStore();
+		return secretRecipeStore;
+	}
+
+	public SecretRecipeStore addRecipe(Recipe recipe, Employee employee){
+		this.store.put(recipe, employee.getPinNumber());
+		return this;
+	}
+
+	public Recipe getRecipe(String name, Employee employee) throws Exception {
+		Recipe recipe = this.findByName(name);
+		if(recipe == null) throw new Exception("Cannot find recipe: " + name);
+		PinNumber recipePin = this.store.get(recipe);
+		if(employee.getPinNumber().getPin() != recipePin.getPin()) throw new Exception("Pins do not match");
+		return recipe;
+	}
+
+
+	private Recipe findByName(String name){
+		Recipe recipe = null;
+		for(Recipe r : this.store.keySet()){
+			if(r.getName().equals(name)){
+				recipe = r;
+				break;
+			}
+		}
+		return recipe;
+	}
 }
--- edu/jalc/inclass/cocacola/recipe/store/SecretRecipeStoreTest.java
+++ edu/jalc/inclass/cocacola/recipe/store/SecretRecipeStoreTest.java
@@ -1,4 +1,67 @@
 package edu.jalc.inclass.cocacola.recipe.store;
 
+import edu.jalc.inclass.cocacola.employee.Employee;
+import edu.jalc.inclass.cocacola.product.ingredient.Ingredient;
+import edu.jalc.inclass.cocacola.recipe.Recipe;
+import edu.jalc.inclass.cocacola.security.PinNumber;
+
 public class SecretRecipeStoreTest {
+
+	public void testGetInstance(){
+		System.out.println("Testing SecretRecipeTest::getInstance");
+
+		SecretRecipeStore store = SecretRecipeStore.getInstance();
+		SecretRecipeStore other = SecretRecipeStore.getInstance();
+		assert(store == other);
+	}
+
+	public void testGetRecipe() throws Exception {
+		System.out.println("Testing SecretRecipeTest::addRecipe");
+
+		SecretRecipeStore store = SecretRecipeStore.getInstance();
+		Recipe recipe = new Recipe("name", "instructions", new Ingredient("ingredient"));
+		PinNumber pinNumber = new PinNumber(1234);
+		Employee employee = new Employee("employee", pinNumber);
+		store.addRecipe(recipe, employee);
+		assert(recipe == store.getRecipe("name", employee));
+	}
+
+
+	public void testGetRecipeWithNoRecipe(){
+		System.out.println("Testing SecretRecipeTest::getRecipe with no recipe");
+		SecretRecipeStore store = SecretRecipeStore.getInstance();
+		boolean exceptionThrown = false;
+		try {
+			store.getRecipe("none-found", null);
+		} catch(Exception e){
+			exceptionThrown = true;
+		}
+		assert(exceptionThrown);
+	}
+
+	public void testGetRecipeWithBadPin(){
+		System.out.println("Testing SecretRecipeTest::getRecipe with bad pin");
+		SecretRecipeStore store = SecretRecipeStore.getInstance();
+		Recipe recipe = new Recipe("name", "instructions", new Ingredient("ingredient"));
+		PinNumber pinNumber = new PinNumber(1234);
+		Employee employee = new Employee("employee", pinNumber);
+		store.addRecipe(recipe, employee);
+		boolean exceptionThrown = false;
+		try {
+			employee.setPinNumber(new PinNumber(2345));
+			store.getRecipe("name", employee);
+		} catch(Exception e){
+			exceptionThrown = true;
+		}
+		assert(exceptionThrown);
+	}
+
+	public static void main(String... args) throws Exception {
+		SecretRecipeStoreTest secretRecipeStoreTest = new SecretRecipeStoreTest();
+		secretRecipeStoreTest.testGetInstance();
+		secretRecipeStoreTest.testGetRecipe();
+		secretRecipeStoreTest.testGetRecipeWithNoRecipe();
+		secretRecipeStoreTest.testGetRecipeWithBadPin();
+		System.out.println("Completed SecretRecipeStore tests");
+	}
 }
