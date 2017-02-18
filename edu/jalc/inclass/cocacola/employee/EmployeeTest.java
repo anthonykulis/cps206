47020380ef32d869ee811e4b78b44c0ec382f59f
ClarkLindsay <clindsay6821@volmail.jalc.edu>
2017-02-19 10:10:09 +1200

    Fleshed out EmployeeTest

--- edu/jalc/inclass/cocacola/employee/EmployeeTest.java
+++ edu/jalc/inclass/cocacola/employee/EmployeeTest.java
@@ -1,44 +1,57 @@
 package edu.jalc.inclass.cocacola.employee;
 
+import edu.jalc.inclass.cocacola.security.PinNumber;
+
 public class EmployeeTest {
 
-	public void testNameOnlyConstructor(){
-		System.out.println("Testing Employee(String name)");
-		assert(false);
-	}
+   public void testNameOnlyConstructor(){
+      System.out.println("Testing Employee(String name)");
+      Employee employee = new Employee("John", null);
+      assert(employee.getName().equals("John"));
+   }
 
-	public void testNameAndPinConstructor(){
-		System.out.println("Testing Employee(String name, PinNumber pinNumber)");
-		assert(false);
-	}
+   public void testNameAndPinConstructor(){
+      System.out.println("Testing Employee(String name, PinNumber pinNumber)");
+      PinNumber pinNumber = new PinNumber(0);
+      Employee employee = new Employee("John", pinNumber);
+      assert(employee.getName().equals("John") && employee.getPinNumber() == pinNumber);
+   }
 
-	public void testSetPinNUmber(){
-		System.out.println("Testing Employee::setPinNumber");
-		assert(false);
-	}
+   public void testSetPinNumber(){
+      System.out.println("Testing Employee::setPinNumber");
+      Employee employee = new Employee("John", null);
+      PinNumber pinNumber = new PinNumber(3);
+      employee.setPinNumber(pinNumber);
+      assert(employee.getPinNumber() == pinNumber);
+   }
 
-	public void testGetName(){
-		System.out.println("Testing Employee::getName");
-		assert(false);
-	}
+   public void testGetName(){
+      System.out.println("Testing Employee::getName");
+      Employee employee = new Employee("John", null);
+      assert(employee.getName().equals("John"));
+   }
 
-	public void testGetPinNumber(){
-		System.out.println("Testing Employee::getPinNumber");
-		assert(false);
-	}
+   public void testGetPinNumber(){
+      System.out.println("Testing Employee::getPinNumber");
+      PinNumber pinNumber = new PinNumber(0);
+      Employee employee = new Employee("John", pinNumber);
+      assert(employee.getPinNumber() == pinNumber);	
+   }
 
-	public void testToString(){
-		System.out.println("Testing Employee::toString");
-		assert(false);
-	}
+   public void testToString(){
+      System.out.println("Testing Employee::toString");
+      PinNumber pinNumber = new PinNumber(0);
+      Employee employee = new Employee("John", pinNumber);
+      assert(employee.toString().equals(employee.getName() + "::" + employee.getPinNumber().toString()));
+   }
 
-	public static void main(String... args){
-		EmployeeTest employeeTest = new EmployeeTest();
-		employeeTest.testNameOnlyConstructor();
-		employeeTest.testNameAndPinConstructor();
-		employeeTest.testSetPinNUmber();
-		employeeTest.testGetName();
-		employeeTest.testGetPinNumber();
-		employeeTest.testToString();
-	}
+   public static void main(String... args){
+      EmployeeTest employeeTest = new EmployeeTest();
+      employeeTest.testNameOnlyConstructor();
+      employeeTest.testNameAndPinConstructor();
+      employeeTest.testSetPinNumber();
+      employeeTest.testGetName();
+      employeeTest.testGetPinNumber();
+      employeeTest.testToString();
+   }
 }
