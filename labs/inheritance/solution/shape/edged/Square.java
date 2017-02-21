47b9c54c3c2c9098b21eddc1cfadb82ffded0c5a
Anthony <anthony.kulis@gmail.com>
2017-02-21 13:17:18 -0600

    finishes up refactor of edged shapes

--- labs/inheritance/solution/shape/edged/Square.java
+++ labs/inheritance/solution/shape/edged/Square.java
@@ -6,13 +6,4 @@ public final class Square extends Rectangle {
 		super(side, side);
 	}
 
-	/* Tests - for equals only - note this is a BAD place to test that class */
-
-	public static void main(String... args){
-		Square square = new Square(1);
-		Square square1 = new Square(2);
-		Square square2 = new Square(1);
-		assert(square.equals(square2));
-		assert(!square.equals(square1));
-	}
 }
---
+++ labs/inheritance/solution/shape/edged/SquareTest.java
@@ -0,0 +1,5 @@
+package labs.inheritance.solution.shape.edged;
+
+public class SquareTest {
+	// no tests needed.
+}
