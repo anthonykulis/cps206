5a678dde6c699e8d759cd38b6d8027f4ccc3de36
Anthony <anthony.kulis@gmail.com>
2017-02-21 12:57:21 -0600

    modifies circle to move test into circletest

--- labs/inheritance/solution/Circle.java
+++ labs/inheritance/solution/Circle.java
@@ -8,26 +8,5 @@ public final class Circle extends Ellipse {
 
 	public final double circumference(){ return (this.getMinorAxis() + this.getMajorAxis()) * Math.PI; }
 
-	/* Tests */
-	void test(){
-		testCircumference();
-		testArea();
-	}
 
-	private void testCircumference(){
-		this.setMinorAxis(3);
-		this.setMajorAxis(3);
-		assert(this.circumference() == (3 + 3) * Math.PI);
-	}
-
-	private void testArea(){
-		this.setMinorAxis(3);
-		this.setMajorAxis(3);
-		assert(this.area() == Math.PI * 3 * 3);
-	}
-
-	public static void main(String... args){
-		Circle c = new Circle(3);
-		c.test();
-	}
 }
---
+++ labs/inheritance/solution/CircleTest.java
@@ -0,0 +1,23 @@
+package labs.inheritance.solution;
+
+public class CircleTest {
+
+	private void testCircumference(){
+		System.out.println("Testing Circle::circumference");
+		Circle circle = new Circle(3);
+		assert(circle.circumference() == (3 + 3) * Math.PI);
+	}
+
+	private void testArea(){
+		System.out.println("Testing Circle::area");
+		Circle circle = new Circle(3);
+		assert(circle.area() == Math.PI * 3 * 3);
+	}
+
+	public static void main(String... args){
+		CircleTest test = new CircleTest();
+		test.testCircumference();
+		test.testArea();
+		System.out.println("Completed Circle tests");
+	}
+}
