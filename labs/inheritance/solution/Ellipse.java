503e29db635039f778449da38484b806c5168fdb
Anthony <anthony.kulis@gmail.com>
2017-02-21 13:01:23 -0600

    moves tests for ellipse of of class into test

--- labs/inheritance/solution/Ellipse.java
+++ labs/inheritance/solution/Ellipse.java
@@ -13,26 +13,5 @@ public class Ellipse extends EllipseShape {
 		return 2 * Math.PI * Math.sqrt((a*a + b*b)/2);
 	}
 
-	/* tests */
-	void test(){
-		testCircumference();
-		testArea();
-	}
 
-	private void testCircumference(){
-		this.setMinorAxis(3);
-		this.setMajorAxis(4);
-		assert(this.circumference() == 2 * Math.PI * Math.sqrt((9.0 + 16.0)/2));
-	}
-
-	private void testArea(){
-		this.setMinorAxis(3);
-		this.setMajorAxis(4);
-		assert(this.area() == Math.PI * 3 * 4);
-	}
-
-	public static void main(String... args){
-		Ellipse ellipse = new Ellipse(3,4);
-		ellipse.test();
-	}
 }
