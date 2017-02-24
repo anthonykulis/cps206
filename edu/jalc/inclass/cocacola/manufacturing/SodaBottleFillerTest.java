3ef6803cf6e382a3032aeb6902e9cb130c0849e6
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-24 12:17:05 -0600

    SodaBottleFillerTest Complete

--- edu/jalc/inclass/cocacola/manufacturing/SodaBottleFillerTest.java
+++ edu/jalc/inclass/cocacola/manufacturing/SodaBottleFillerTest.java
@@ -1,18 +1,30 @@
 package edu.jalc.inclass.cocacola.manufacturing;
 
+import edu.jalc.inclass.cocacola.material.bottle.SodaBottle;
+import edu.jalc.inclass.cocacola.material.label.SodaBottleLabel;
+import edu.jalc.inclass.cocacola.material.lid.SodaBottleLid;
+import edu.jalc.inclass.cocacola.product.liquid.Soda;
+
 public class SodaBottleFillerTest {
 
 	public void testGetInstance(){
 		System.out.println("Testing SodaBottleFiller::getInstance");
-		assert(false);
+		SodaBottleFiller sodaBottleFiller = SodaBottleFiller.getInstance();
+		assert(sodaBottleFiller == sodaBottleFiller.getInstance());
 	}
 
-	public void testFillSoda(){
+	public void testFillSoda() throws Exception{
 		System.out.println("Testing SodaBottleFiller::fillSoda");
-		assert(false);
+		SodaBottleLid sodaBottleLid = new SodaBottleLid(1,1);
+		Soda soda = new Soda(0.0);
+		SodaBottle sodaBottle = new SodaBottle(0.0, sodaBottleLid, soda, null);
+		sodaBottle.setSoda(soda);
+		SodaBottleFiller sodaBottleFiller = SodaBottleFiller.getInstance();
+		sodaBottleFiller.fillSoda(sodaBottle);
+		assert(soda.getVolume() == sodaBottle.getCapacity());
 	}
 
-	public static void main(String... args){
+	public static void main(String... args) throws Exception{
 		SodaBottleFillerTest sodaBottleFillerTest = new SodaBottleFillerTest();
 		sodaBottleFillerTest.testGetInstance();
 		sodaBottleFillerTest.testFillSoda();
