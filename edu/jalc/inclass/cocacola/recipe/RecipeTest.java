61a1bccb05a1a73e29cf98a71fc59ae037763855
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-24 11:38:02 -0600

    Complete RecipeTest

--- edu/jalc/inclass/cocacola/recipe/RecipeTest.java
+++ edu/jalc/inclass/cocacola/recipe/RecipeTest.java
@@ -1,25 +1,42 @@
 package edu.jalc.inclass.cocacola.recipe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import edu.jalc.inclass.cocacola.product.ingredient.Ingredient;
+
 public class RecipeTest {
 
 	public void testGetIngredients(){
 		System.out.println("Testing Recipe::getIngredients");
-		assert(false);
+		ArrayList<Ingredient> ingredients = new ArrayList<>();
+		Ingredient[] ingredient = new Ingredient[3];
+		ingredient[0] = new Ingredient("milk");
+		ingredient[1] = new Ingredient("cat");
+		ingredient[2] = new Ingredient("salt");
+		ingredients.addAll(Arrays.asList(ingredient));
+		Recipe recipe = new Recipe("food", "Burn it", ingredient);
+		assert(recipe.getIngredients().equals(ingredients));
 	}
 
 	public void testGetName(){
 		System.out.println("Testing Recipe::getName");
-		assert(false);
+		Ingredient ingredient = new Ingredient("salt");
+		Recipe recipe = new Recipe("food", "Burn it",ingredient);
+		assert(recipe.getName() == "food");
 	}
 
 	public void testGetInstructions(){
 		System.out.println("Testing Recipe::getInstructions");
-		assert(false);
+		Ingredient ingredient = new Ingredient("salt");
+		Recipe recipe = new Recipe("food", "Burn it", ingredient);
+		assert(recipe.getInstructions() == "Burn it");
 	}
 
 	public void testToString(){
 		System.out.println("Testing Recipe::toString");
-		assert(false);
+		Ingredient ingredient = new Ingredient("salt");
+		Recipe recipe = new Recipe("food", "Burn it", ingredient);
+		assert(recipe.toString().equals(recipe.getName() + " - " + recipe.getInstructions()));
 	}
 
 	public static void main(String... args){
