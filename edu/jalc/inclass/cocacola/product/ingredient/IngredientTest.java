ed05f941e1d48b965bb484b45945d1246d38f316
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-02-24 19:41:07 -0600

    again fixed tabs

--- edu/jalc/inclass/cocacola/product/ingredient/IngredientTest.java
+++ edu/jalc/inclass/cocacola/product/ingredient/IngredientTest.java
@@ -2,22 +2,22 @@ package edu.jalc.inclass.cocacola.product.ingredient;
 
 public class IngredientTest {
 
-	public void testGetName(){
-   	System.out.println("Testing Ingredient::getName");
+   public void testGetName(){
+      System.out.println("Testing Ingredient::getName");
       Ingredient ingredient = new Ingredient("carbonated water");
-		assert(ingredient.getName().equals("carbonated water"));
-	}
+      assert(ingredient.getName().equals("carbonated water"));
+   }
 
-	public void testtoString(){
-		System.out.println("Testing Ingredient::getName");
+   public void testtoString(){
+      System.out.println("Testing Ingredient::getName");
       Ingredient ingredient = new Ingredient("carbonated water");
-		assert(ingredient.toString().equals("carbonated water"));
-	}
+      assert(ingredient.toString().equals("carbonated water"));
+   }
 
-	public static void main(String... args){
-		IngredientTest ingredientTest = new IngredientTest();
-		ingredientTest.testGetName();
-		ingredientTest.testtoString();
-		System.out.println("Completed testing Ingredients");
-	}
+   public static void main(String... args){
+      IngredientTest ingredientTest = new IngredientTest();
+      ingredientTest.testGetName();
+      ingredientTest.testtoString();
+      System.out.println("Completed testing Ingredients");
+   }
 }
