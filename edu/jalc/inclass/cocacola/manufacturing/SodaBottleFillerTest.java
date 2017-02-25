1d29caf5e2c3dc2e4538152050bedf8cb48844bb
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-02-24 19:11:19 -0600

    sodaBottleFiller Test

--- edu/jalc/inclass/cocacola/manufacturing/SodaBottleFillerTest.java
+++ edu/jalc/inclass/cocacola/manufacturing/SodaBottleFillerTest.java
@@ -1,21 +1,32 @@
 package edu.jalc.inclass.cocacola.manufacturing;
 
+import edu.jalc.inclass.cocacola.material.bottle.SodaBottle;
+import edu.jalc.inclass.cocacola.material.lid.SodaBottleLid;
+import edu.jalc.inclass.cocacola.product.liquid.Soda;
+
 public class SodaBottleFillerTest {
 
-	public void testGetInstance(){
-		System.out.println("Testing SodaBottleFiller::getInstance");
-		assert(false);
-	}
+   public void testGetInstance(){
+      System.out.println("Testing SodaBottleFiller::getInstance");
+      SodaBottleFiller sodaBottleFiller =  SodaBottleFiller.getInstance();
+      assert(sodaBottleFiller.getInstance()== sodaBottleFiller  );
+   }
 
-	public void testFillSoda(){
-		System.out.println("Testing SodaBottleFiller::fillSoda");
-		assert(false);
-	}
+   public void testFillSoda()throws Exception{
+      System.out.println("Testing SodaBottleFiller::fillSoda");
+   
+      Soda soda = new Soda(10);
+      SodaBottleLid lid = new SodaBottleLid(0,0); 
+      SodaBottle sodaBottle = new SodaBottle(10.0,lid,soda, null);
+      SodaBottleFiller sodaBottleFiller = SodaBottleFiller.getInstance();
+      sodaBottleFiller.fillSoda(sodaBottle);
+      assert(soda.getVolume() == sodaBottle.getCapacity());
+   }
 
-	public static void main(String... args){
-		SodaBottleFillerTest sodaBottleFillerTest = new SodaBottleFillerTest();
-		sodaBottleFillerTest.testGetInstance();
-		sodaBottleFillerTest.testFillSoda();
-		System.out.println("Completed testing SodaBottleFiller");
-	}
+   public static void main(String... args)throws Exception{
+      SodaBottleFillerTest sodaBottleFillerTest = new SodaBottleFillerTest();
+      sodaBottleFillerTest.testGetInstance();
+      sodaBottleFillerTest.testFillSoda();
+      System.out.println("Completed testing SodaBottleFiller");
+   }
 }
