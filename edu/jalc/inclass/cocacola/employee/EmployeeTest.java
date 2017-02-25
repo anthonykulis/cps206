48e861d7b4632c2d96de23e558d45d08b42f351a
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-02-24 19:09:55 -0600

    created employeeTest

--- edu/jalc/inclass/cocacola/employee/EmployeeTest.java
+++ edu/jalc/inclass/cocacola/employee/EmployeeTest.java
@@ -1,35 +1,49 @@
 package edu.jalc.inclass.cocacola.employee;
 
+import edu.jalc.inclass.cocacola.security.PinNumber;
+
 public class EmployeeTest {
 
 	public void testNameOnlyConstructor(){
 		System.out.println("Testing Employee(String name)");
-		assert(false);
+      Employee employee = new Employee("Nour");
+      assert(employee.getName().equals("Nour"));
 	}
 
 	public void testNameAndPinConstructor(){
 		System.out.println("Testing Employee(String name, PinNumber pinNumber)");
-		assert(false);
+       PinNumber pinNumber = new PinNumber(8);
+       Employee employee = new Employee("Nour",pinNumber);
+      assert(employee.getName().equals("Nour")&& employee.getPinNumber()== pinNumber);
 	}
-
+	
 	public void testSetPinNUmber(){
 		System.out.println("Testing Employee::setPinNumber");
-		assert(false);
+      PinNumber pinNumber =new PinNumber(7);
+      Employee employee = new Employee("Nour",pinNumber);
+      employee.setPinNumber(pinNumber);
+		assert(employee.getPinNumber() == pinNumber);
 	}
 
 	public void testGetName(){
 		System.out.println("Testing Employee::getName");
-		assert(false);
+       Employee employee = new Employee("Nour");
+		assert(employee.getName().equals("Nour"));
 	}
 
 	public void testGetPinNumber(){
 		System.out.println("Testing Employee::getPinNumber");
-		assert(false);
+      PinNumber pinNumber =new PinNumber(7);
+      Employee employee = new Employee("Nour",pinNumber);
+		assert(employee.getPinNumber() == pinNumber);
+		
 	}
 
 	public void testToString(){
 		System.out.println("Testing Employee::toString");
-		assert(false);
+      PinNumber pinNumber =new PinNumber(7);
+      Employee employee = new Employee("Nour",pinNumber);
+		assert(employee.toString().equals(employee.getName() + "::" + employee.getPinNumber().toString()));
 	}
 
 	public static void main(String... args){
